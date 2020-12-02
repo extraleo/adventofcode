ca8a958b61791ac47e4d64d2bdb2cd29a9d28e10
leih <ray.leiheng@qq.com>
2020-12-02 17:05:03 +0800

    y2020-day1

---
+++ advent/code/y2020/day1/day1.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"extraleo/algorithm/utils"
+	"fmt"
+)
+
+func day() int {
+	data := utils.SpiltInputGetInt(utils.INPUT, "\n")
+	dataMap := make(map[int]bool)
+	for _, item := range data {
+		dataMap[item] = true
+	}
+	for _, item := range data {
+		if dataMap[2020-item] {
+			return item * (2020 - item)
+		}
+	}
+	return -1
+}
+
+func night() int {
+	data := utils.SpiltInputGetInt(utils.INPUT, "\n")
+	dataMap := make(map[int]bool)
+	for _, item := range data {
+		dataMap[item] = true
+	}
+	for _,item :=range data{
+		second := 2020 - item
+		for _, k := range data{
+			if dataMap[(second-k)]{
+				fmt.Print("Item: ,K, k-second:",item, k, second-k)
+				return k*(second-k)*item
+			}
+		}
+	}
+	return -1
+}
+
+func main() {
+	fmt.Println("Night: ", night())
+}
