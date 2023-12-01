bb827d649fc4f0f48c4a848cc2eac015e4c295ba
extraleo <heng.lei@microfocus.com>
2023-12-02 02:11:27 +0800

    update

--- advent/code/y2019/day5/day5.go
+++ advent/code/y2019/day5/day5.go
@@ -1,40 +1,144 @@
 package main
 
-import "adventofcode/utils"
+import (
+	"adventofcode/utils"
+	"bufio"
+	"fmt"
+	"os"
+)
 
-func day(noun, verb int) int {
-	data := utils.SpiltInputGetInt(utils.INPUT, ",")
-	data[1] = noun
-	data[2] = verb
-	source := extendAndCopy(data, len(data))
-	for index := 0; index < len(source); {
-		switch source[index] {
+func day5() []int {
+	inputs:=utils.SpiltInputGetInt("input.txt", ",")
+	for address := 0; address < len(inputs); address++ {
+		opcode := inputs[address] % 100
+		mode1st := 0
+		mode2nd := 0
+		if inputs[address] >= 100 {
+			if ((inputs[address] % 1000) - (inputs[address] % 100)) == 100 {
+				mode1st = 1
+			}
+			if ((inputs[address] % 10000) - (inputs[address] % 1000)) == 1000 {
+				mode2nd = 1
+			}
+		}
+
+		switch opcode {
 		case 1:
-			source[source[index+3]] = source[source[index+1]] + source[source[index+2]]
-			index = index + 4
+			// +
+			parameter1 := inputs[address+1]
+			if mode1st == 0 {
+				parameter1 = inputs[parameter1]
+			}
+			parameter2 := inputs[address+2]
+			if mode2nd == 0 {
+				parameter2 = inputs[parameter2]
+			}
+
+			inputs[inputs[address+3]] = parameter1 + parameter2
+			address = address + 3
 		case 2:
-			source[source[index+3]] = source[source[index+1]] * source[source[index+2]]
-			index = index + 4
+			// *
+			parameter1 := inputs[address+1]
+			if mode1st == 0 {
+				parameter1 = inputs[parameter1]
+			}
+			parameter2 := inputs[address+2]
+			if mode2nd == 0 {
+				parameter2 = inputs[parameter2]
+			}
+
+			inputs[inputs[address+3]] = parameter1 * parameter2
+			address = address + 3
+		case 3:
+			// Read
+			scanner := bufio.NewScanner(os.Stdin)
+			for {
+				fmt.Print("Input: ")
+				scanner.Scan()
+				text := scanner.Text()
+				if len(text) != 0 {
+					inputs[inputs[address+1]] = utils.Atoi(text)
+					break
+				}
+
+			}
+			address = address + 1
+		case 4:
+			// Write
+			fmt.Printf("(%d)\n", inputs[inputs[address+1]])
+			address = address + 1
+		case 5:
+			// Jump-if-true
+			parameter1 := inputs[address+1]
+			if mode1st == 0 {
+				parameter1 = inputs[parameter1]
+			}
+			parameter2 := inputs[address+2]
+			if mode2nd == 0 {
+				parameter2 = inputs[parameter2]
+			}
+			if parameter1 != 0 {
+				address = parameter2 - 1 
+				continue
+			}
+			address = address + 2
+		case 6:
+			// Jump-if-false
+			parameter1 := inputs[address+1]
+			if mode1st == 0 {
+				parameter1 = inputs[parameter1]
+			}
+			parameter2 := inputs[address+2]
+			if mode2nd == 0 {
+				parameter2 = inputs[parameter2]
+			}
+
+			if parameter1 == 0 {
+				address = parameter2 - 1
+				continue
+			}
+			address = address + 2
+		case 7:
+			// less-than
+			parameter1 := inputs[address+1]
+			if mode1st == 0 {
+				parameter1 = inputs[parameter1]
+			}
+			parameter2 := inputs[address+2]
+			if mode2nd == 0 {
+				parameter2 = inputs[parameter2]
+			}
+			inputs[inputs[address+3]] = 0
+			if parameter1 < parameter2 {
+				inputs[inputs[address+3]] = 1
+			}
+			address = address + 3
+		case 8:
+			// equals
+			parameter1 := inputs[address+1]
+			if mode1st == 0 {
+				parameter1 = inputs[parameter1]
+			}
+			parameter2 := inputs[address+2]
+			if mode2nd == 0 {
+				parameter2 = inputs[parameter2]
+			}
+
+			inputs[inputs[address+3]] = 0
+			if parameter1 == parameter2 {
+				inputs[inputs[address+3]] = 1
+			}
+			address = address + 3
 		case 99:
-			// fmt.Println("The result: ", source[0])
-			return source[0]
-		default:
-			index++
+			// Halt
+			return inputs
 		}
 	}
-	return -1
+
+	return inputs
 }
 
-func extendAndCopy(source []int, len int) []int {
-	target := make([]int, len+1)
-	copy(target, source)
-	return target
-}
 
-func night(){
-
-}
-
-func main()  {
-	
+func main() {
+	day5()
 }
