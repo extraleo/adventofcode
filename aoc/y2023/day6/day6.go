0f263f55648a9ae74c87b497bd7cfe932f467603
extraleo <ray.leiheng@qq.com>
2023-12-08 18:58:13 +0800

    save

--- aoc/y2023/day6/day6.go
+++ aoc/y2023/day6/day6.go
@@ -26,12 +26,34 @@ func day() {
 	fmt.Println("day:", result)
 }
 
-func 
+func night(){
+	lines := utils.SpiltInputByLine("input.txt")
+	timesTmp := utils.ConvertIntString2String(strings.Split(lines[0], ":")[1])
+	distancesTmp := utils.ConvertIntString2String(strings.Split(lines[1], ":")[1])
+	timeStr:=""
+	disStr:=""
+	for index := range timesTmp{
+		timeStr=timeStr+timesTmp[index]
+		disStr=disStr+distancesTmp[index]
+	}
+	result := 1
+		count := 0
+		time:=utils.Atoi(timeStr)
+		for hold := 0; hold <= time; hold++ {
+			if race(hold, time) > utils.Atoi(disStr) {
+				count++
+			}
+		}
+		if (count>0){
+			result = result * count
+		}
+	fmt.Println("night:", result)
+}
 
 func race(hold, total int) int {
 	return hold * (total - hold)
 }
 
 func main() {
-	day()
+	night()
 }
