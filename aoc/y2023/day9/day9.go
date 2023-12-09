565d7da628a72e847325cb687d8b42302d45bcc9
extraleo <ray.leiheng@qq.com>
2023-12-09 22:58:28 +0800

    format

--- aoc/y2023/day9/day9.go
+++ aoc/y2023/day9/day9.go
@@ -9,52 +9,50 @@ import (
 //go:embed input.txt
 var input string
 
-func day(){
-	result:=0
-	lines:=utils.SpiltInputGetIntList(input, ` `)
-	for _,line:=range lines{
-		result+=findExtrapolated(line)
+func day() {
+	result := 0
+	lines := utils.SpiltInputGetIntList(input, ` `)
+	for _, line := range lines {
+		result += findExtrapolated(line)
 	}
 	fmt.Println("day:", result)
 }
 
-func findExtrapolated(history []int) int{
-	if (history[0] == history[len(history)-1]){
+func findExtrapolated(history []int) int {
+	if history[0] == history[len(history)-1] {
 		return history[len(history)-1]
 	}
-	next:=getNext(history)
+	next := getNext(history)
 	return findExtrapolated(next) + history[len(history)-1]
 
 }
 
-func findPreExtrapolated(history []int) int{
-	if (history[0] == history[len(history)-1]){
+func findPreExtrapolated(history []int) int {
+	if history[0] == history[len(history)-1] {
 		return history[len(history)-1]
 	}
-	next:=getNext(history)
-	return  history[0] - findPreExtrapolated(next)
+	next := getNext(history)
+	return history[0] - findPreExtrapolated(next)
 
 }
 
-
-
-func getNext(history []int) []int{
-	result:=[]int{}
-	for i:=1; i<len(history);i++{
+func getNext(history []int) []int {
+	result := []int{}
+	for i := 1; i < len(history); i++ {
 		result = append(result, history[i]-history[i-1])
 	}
 	return result
 }
 
-func night(){
-	result:=0
-	lines:=utils.SpiltInputGetIntList(input, ` `)
-	for _,line:=range lines{
-		result+=findPreExtrapolated(line)
+func night() {
+	result := 0
+	lines := utils.SpiltInputGetIntList(input, ` `)
+	for _, line := range lines {
+		result += findPreExtrapolated(line)
 	}
 	fmt.Println("night:", result)
 }
 
-func main(){
+func main() {
 	night()
 }
