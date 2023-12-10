bb67ee47dcf9ee74ce1b92dd82f98f82ef65e88f
extraleo <ray.leiheng@qq.com>
2023-12-10 14:06:27 +0800

    save

---
+++ aoc/y2023/day10/day10.go
@@ -0,0 +1,19 @@
+package main
+
+
+func main(){
+	day()
+	night()
+}
+
+func findCycle(){
+	panic("unimplemented")
+}
+
+func night() {
+	panic("unimplemented")
+}
+
+func day() {
+	panic("unimplemented")
+}
