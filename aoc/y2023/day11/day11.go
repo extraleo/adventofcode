1254a55486f1fdefa96cf998cd020a3a5de4e376
extraleo <ray.leiheng@qq.com>
2023-12-11 23:49:24 +0800

    save

--- aoc/y2023/day11/day11.go
+++ aoc/y2023/day11/day11.go
@@ -10,7 +10,7 @@ import (
 //go:embed input.txt
 var input string
 
-func buildGrid(lines []string) (grid models.Grid) {
+func buildGrid(lines []string, expand bool) (grid models.Grid, offset [][]int) {
 	grid = make(models.Grid)
 	upX := make([]int, len(lines[0]))
 	upY := make([]int, len(lines))
@@ -27,28 +27,32 @@ func buildGrid(lines []string) (grid models.Grid) {
 		}
 	}
 
-	offsetY := 0
-	for y := range upY {
-		if upY[y] == 1 {
-			empty := lines[y+offsetY]
-			lines = append(lines[:y+offsetY+1], lines[y+offsetY:]...)
-			lines[y+offsetY+1] = empty
-			offsetY++
-		}
-	}
-
-	offsetX := 0
-	for x := range upX {
-		if upX[x] == 0 {
-			for i, line := range lines {
-				index := x + offsetX
-				newLine := line[:index] + "." + line[index:]
-				lines[i] = newLine
+	offset = append(offset, upX, upY)
+	if expand {
+		offsetY := 0
+		for y := range upY {
+			if upY[y] == 1 {
+				empty := lines[y+offsetY]
+				lines = append(lines[:y+offsetY+1], lines[y+offsetY:]...)
+				lines[y+offsetY+1] = empty
+				offsetY++
 			}
-			offsetX++
+		}
 
+		offsetX := 0
+		for x := range upX {
+			if upX[x] == 0 {
+				for i, line := range lines {
+					index := x + offsetX
+					newLine := line[:index] + "." + line[index:]
+					lines[i] = newLine
+				}
+				offsetX++
+
+			}
 		}
 	}
+
 	count := 1
 	for j, l := range lines {
 		for i, c := range l {
@@ -66,7 +70,7 @@ func day() {
 	input = strings.TrimSuffix(input, "\n")
 	lines := strings.Split(input, "\n")
 
-	grid := buildGrid(lines)
+	grid, _ := buildGrid(lines, true)
 
 	steps := 0
 	hasCalc := make(map[int]bool)
@@ -86,6 +90,56 @@ func day() {
 
 }
 
+func night() {
+	input = strings.TrimSuffix(input, "\n")
+	lines := strings.Split(input, "\n")
+
+	grid, offset := buildGrid(lines, false)
+	offsetX := offset[0]
+	offsetY := offset[1]
+	steps := 0
+	hasCalc := make(map[int]bool)
+	for k, v := range grid {
+		for ik, iv := range grid {
+			if hasCalc[v] || hasCalc[iv] || iv == v {
+			} else {
+				xStart, xEnd, yStart, yEnd := 0, 0, 0, 0
+				if k.X > ik.X {
+					xStart, xEnd = ik.X, k.X
+				} else {
+					xStart, xEnd = k.X, ik.X
+				}
+
+				if k.Y > ik.Y {
+					yStart, yEnd = ik.Y, k.Y
+				} else {
+					yStart, yEnd = k.Y, ik.Y
+				}
+
+				offsX := 0
+				for i := xStart; i < xEnd; i++ {
+					if offsetX[i] == 0 {
+						offsX++
+					}
+				}
+				offsY := 0
+				for y := yStart; y < yEnd; y++ {
+					if offsetY[y] == 1 {
+						offsY++
+					}
+				}
+
+				steps += xEnd + offsX*(1000000-1) - xStart + yEnd + offsY*(1000000-1) - yStart
+
+			}
+		}
+		hasCalc[v] = true
+	}
+
+	fmt.Println("night:", steps)
+}
+
 func main() {
-	day()
+	// day()
+	night()
 }
