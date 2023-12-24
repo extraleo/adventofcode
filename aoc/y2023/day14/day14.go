19adb22a0bc6aa15936043c1be54060540088152
extraleo <ray.leiheng@qq.com>
2023-12-24 22:48:54 +0800

    day14-1

--- aoc/y2023/day14/day14.go
+++ aoc/y2023/day14/day14.go
@@ -1,13 +1,17 @@
 package main
 
-import _ "embed"
+import (
+	"adventofcode/utils"
+	_ "embed"
+	"fmt"
+)
 
-//go:embed input-test.txt
+//go:embed input.txt
 var input string
 
 func main() {
 	day()
-	night()
+	// night()
 }
 
 func night() {
@@ -15,5 +19,27 @@ func night() {
 }
 
 func day() {
-	panic("unimplemented")
+	parts := utils.ParseAsMatrixChars(input)
+	grid := parts[0]
+	loads := make([]int, len(grid[0]))
+
+	n := len(grid)
+	for i, _ := range loads {
+		loads[i] = n
+	}
+
+	sum := 0
+
+	for i, r := range grid {
+		for j, v := range r {
+			if v == 'O' {
+				sum += loads[j]
+				loads[j]--
+			}
+			if v == '#' {
+				loads[j] = n - i - 1
+			}
+		}
+	}
+	fmt.Println("day", sum)
 }
