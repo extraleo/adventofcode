d40b3c3ad2c912c6a02fb0a8f0f2d6dac920da60
extraleo <ray.leiheng@qq.com>
2023-12-24 21:46:52 +0800

    day13

--- aoc/y2023/day13/day13.go
+++ aoc/y2023/day13/day13.go
@@ -1,45 +1,128 @@
 package main
 
-import _ "embed"
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+)
 
-//go:embed input-test.txt
+//go:embed input.txt
 var input string
 
 type pattern [][]rune
 
 func main() {
-	day()
+	// day()
 	night()
 }
 
 func night() {
-	panic("unimplemented")
+	parts := parse()
+	result := 0
+	for _, part := range parts {
+		idx, isHorizontal := findMirror(mapToCount(part))
+		result += calculate(idx, isHorizontal)
+	}
+	fmt.Println("night", result)
 }
 
 func day() {
-	panic("unimplemented")
+	parts := parse()
+	result := 0
+	for _, part := range parts {
+		idx, isHorizontal := findMirror(mapToCount(part))
+		result += calculate(idx, isHorizontal)
+	}
+	fmt.Println("day", result)
+}
+
+func getPattern(part string) pattern {
+	lines := strings.Split(part, "\n")
+
+	var pattern pattern
+	for _, l := range lines {
+		pattern = append(pattern, []rune(l))
+	}
+	return pattern
 }
 
 func parse() []pattern {
-	res := make([]pattern, 0)
-	return res
+	parts := strings.Split(input, "\n\n")
+
+	var patterns []pattern
+	for _, b := range parts {
+		patterns = append(patterns, getPattern(b))
+	}
+	return patterns
 }
 
-func findMirror(pat pattern) (int, bool) {
-	return -1, false
+func findMirror(rowCnt, colCnt []int) (int, bool) {
+	if r := findReflection(rowCnt); r >= 0 {
+		return r + 1, true
+	}
+	return findReflection(colCnt) + 1, false
 }
 
-func findVertical(pat pattern) int {
+func findReflection(cnts []int) int {
+	for i := 1; i < len(cnts); i++ {
+		if isReflection(cnts, i-1, i) {
+			return i - 1
+		}
+	}
 	return -1
 }
 
-func findHorizontal(pat pattern) int {
-	return -1
+func isReflection(cnts []int, l, r int) bool {
+	smudges:=0
+	for l >= 0 && r < len(cnts){
+		ok, smugde :=smudge(cnts[l], cnts[r])
+		if (ok){
+			smudges += smugde
+			l--
+			r++
+		}else{
+			break
+		}
+		
+	}
+	// if part1, remove smudges == 1 condition
+	return (l < 0 || r == len(cnts)) && smudges == 1
+}
+
+func smudge(l, r int) (bool, int) {
+	res := l ^ r
+	if res == 0 {
+		return true,0
+	}
+	if (res & (res - 1)) == 0 {
+		return true,1
+	}
+	return false,0
+}
+
+func mapToCount(pat pattern) ([]int, []int) {
+	rowCnt := make([]int, len(pat))
+	colCnt := make([]int, len(pat[0]))
+
+	for i, r := range pat {
+		for j, v := range r {
+			if v == '#' {
+				rowCnt[i] |= 1 << j
+				colCnt[j] |= 1 << i
+			}
+		}
+	}
+	/*
+		Counts Result for example input
+		RowCnt- [205 180 259 259 180 204 181] ColCnt- [77 12 115 33 82 82 33 115 12]
+		RowCnt- [305 289 460 223 223 460 289] ColCnt- [91 24 60 60 25 67 60 60 103]
+	*/
+	return rowCnt, colCnt
 }
 
 func calculate(idx int, isHorizontal bool) int {
 	if isHorizontal {
-		return 100 * (idx + 1)
+		return 100 * idx
 	}
-	return idx + 1
+	return idx
 }
