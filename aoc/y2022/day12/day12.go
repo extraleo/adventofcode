aeff3715b2c120e6e7dbc075bdb771c07254e7ff
extraleo <ray.leiheng@qq.com>
2024-01-01 22:40:21 +0800

    save

---
+++ aoc/y2022/day12/day12.go
@@ -0,0 +1,19 @@
+package day12
+
+import _ "embed"
+
+//go:embed input-test.txt
+var input string
+
+func main(){
+	day()
+	night()
+}
+
+func day() {
+	panic("unimplemented")
+}
+
+func night() {
+	panic("unimplemented")
+}
