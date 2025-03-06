05f338ce2caa5d043b00694cb9f64b8358410b19
extraleo <ray.leiheng@qq.com>
2025-03-06 17:54:37 +0800

    26d6

--- aoc/y2024/day6/day6.go
+++ aoc/y2024/day6/day6.go
@@ -9,19 +9,69 @@ import (
 //go:embed sample.txt
 var input string
 
+var offsetStream = []Position{
+	{X:-1,Y:0},
+	{X:0,Y:1},
+	{X:1,Y:0},
+	{X:0,Y:-1},
+}
+
 func main(){
 	day()
 	night()
 }
 
+
+
 func day(){
 	grid := ParseGrid(input)
+	repeat, pathLen := trackGuard(grid)
+	fmt.Println("day: ",repeat, len(pathLen))
+}
+
+func trackGuard(grid map[Position]rune) (int, map[Position]Position){
 	guard := getGuardPosition(grid)
-	fmt.Println(guard)
+	turnRight := 0
+	offset := getOffset(turnRight, offsetStream)
+	visited := make(map[Position]Position)
+	visited[guard]=offset
+	repeat := 0
+	for ;; {
+		nextGuard := nextGuard(guard, offset)
+		if v, ok := grid[nextGuard]; ok{
+			if v == '#'{
+				turnRight++
+				offset = getOffset(turnRight, offsetStream)
+			}else{
+				if offsetVisited, vOk:=visited[nextGuard]; !vOk {
+					visited[nextGuard] = offset
+				}else{
+					if offsetVisited != offset{
+						repeat++
+						fmt.Println(nextGuard, offset)
+					}
+				}
+				guard = nextGuard
+			}
+		}else{
+			return repeat, visited
+		}
+	}
+}
+
+func nextGuard(guard, offset Position) Position{
+	guard.X = guard.X + offset.X
+	guard.Y = guard.Y + offset.Y
+	return guard
+}
+
+func getOffset(turnRight int, offsetStream []Position) Position{
+	 p := offsetStream[turnRight%4]
+	 return p
 }
 
 func night(){
-
+	fmt.Println("night: ")
 }
 
 func getGuardPosition(grid map[Position]rune) Position{
--- utils/models/position.go
+++ utils/models/position.go
@@ -1,12 +1,30 @@
 package models
 
-import "adventofcode/utils"
+import (
+	"adventofcode/utils"
+	"strings"
+)
 
 type Position struct{
 	X, Y int
 }
 
+
 func (pos *Position) Manhattan(p1 Position) int{
 	return utils.Abs(pos.X - p1.X) + utils.Abs(pos.Y - p1.Y)
 }
 
+
+
+
+func ParseGrid(str string) map[Position]rune{
+	lines:=strings.Split(str, "\n")
+	result := make(map[Position]rune)
+	for i,l:=range(lines){
+		for j,r:=range(l){
+				p := Position{X: i,Y: j}
+				result[p] = r
+			}
+		}
+	return result
+}
