ec33a8c870bf366c0e6c4a3b093ac11d273555b3
extraleo <ray.leiheng@qq.com>
2025-04-22 14:06:19 +0800

    save

---
+++ leetcode/doublePoints/1456.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+func maxVowels(s string, k int) int {
+	max := 0
+	old := 0
+	for i:=0; i< len(s); i++ {
+		toAdd := 0
+		toMin :=0
+		in := i
+		out := in - k
+		
+		if isVowels(s[in]){
+			toAdd=1
+		}
+
+		if out >=0 && isVowels(s[out]){
+			toMin = -1
+		}
+		
+	old = old + toAdd + toMin
+		max = maxMax(old, max)
+		if max == k {
+			return max
+		}
+
+	}
+	return max
+}
+
+func isVowels(r byte) bool{
+	return r == 'a' || r == 'e' || r == 'i' || r == 'o' || r == 'u'
+}
+
+func maxMax(a,b int) int{
+	if a > b {
+		return a
+
+	}
+	return b
+}
+
+
+// func main(){
+// 	fmt.Println(maxVowels("leetcode", 3))
+// }
