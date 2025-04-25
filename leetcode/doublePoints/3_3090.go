03c6ed982efcf0f9c407db03e1878aa5961340a5
extraleo <ray.leiheng@qq.com>
2025-04-25 17:06:54 +0800

    save

---
+++ leetcode/doublePoints/3_3090.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"adventofcode/leetcode/utils"
+	"fmt"
+)
+
+func lengthOfLongestSubstring(s string) int {
+
+	return maxLengthSubstrings(s, 1)
+
+}
+
+func maximumLengthSubstring(s string) int {
+	return maxLengthSubstrings(s, 2)
+}
+
+// m == repeat count
+func maxLengthSubstrings(s string, m int) int {
+  start, max := 0, 0
+	vMap := map[byte]int{}
+	for i := 0; i< len(s); i++{
+		// if exist
+		if v, ok := vMap[s[i]]; ok {
+			// plus 1
+			vMap[s[i]] = v + 1
+			if vMap[s[i]] >= m {
+				vMap[s[start]] = vMap[s[start]] -1
+				max = utils.MaxInt(max, i-start)
+				start++
+				continue
+			}
+		}
+		vMap[s[i]] = 1
+		max = utils.MaxInt(max, i-start+1)
+	}
+	return max
+}
+
+// deprecated
+// 3 solution
+func firstEdition(s string) int {
+	start := 0
+	max := 0
+	vMap := make(map[byte]int, len(s))
+	for i := 0; i < len(s); i++ {
+		if v, ok := vMap[s[i]]; ok {
+			if v < start {
+				max = utils.MaxInt(max, i-start+1)
+				vMap[s[i]] = i
+				continue
+			}
+			max = utils.MaxInt(max, i-start)
+			start = v + 1
+			vMap[s[i]] = i
+			continue
+		}
+		vMap[s[i]] = i
+		max = utils.MaxInt(max, i-start+1)
+	}
+	return max
+}
+
+
+
+
+func main() {
+	fmt.Println(lengthOfLongestSubstring("pwwkew"))
+}
--- leetcode/doublePoints/README.md
+++ leetcode/doublePoints/README.md
@@ -6,7 +6,7 @@ https://leetcode.cn/discuss/post/3578981/ti-dan-hua-dong-chuang-kou-ding-chang-b
 ## 定长滑动窗口
 ### 基础
 > 核心思想是 +1 和 -1, 因为窗口滑动的过程中, 中间的数是不变的
-> 其中滑动窗口开始循环的下标, 既可以从 0 开始, 也可以从k 开始, 从k 开始的话, 就要先计算好一个base值, 在这个值上进行 +1 和 -1 的操作
+> 其中滑动窗口开始循环的下标, 既可以从 0 开始, 也可以从k 开始, 从k 开始的话, 就要先计算好一个base值, 在这个值上进行 +1 和 -1 的操作. insert -> update -> delete
 > https://leetcode.cn/problems/maximum-number-of-vowels-in-a-substring-of-given-length/solutions/2809359/tao-lu-jiao-ni-jie-jue-ding-chang-hua-ch-fzfo/
 
 - 1456 maximum-number-of-vowels-in-a-substring-of-given-length medium
@@ -20,9 +20,11 @@ https://leetcode.cn/discuss/post/3578981/ti-dan-hua-dong-chuang-kou-ding-chang-b
   1. 环形数组的滑动窗口
   2. 计算长为 n−k 的连续子数组和的最小值
 
-### 进阶 - 选做
+### 进阶 - 选做 TBD
 
 ## 不定长滑动窗口
 只要是连续的子数组, 都可以用滑动窗口
 ### 基础
 - 3 longest-substring-without-repeating-characters
+- 3090 maximum-length-substring-with-two-occurrences
+  - 和 3 是异曲同工, 也和 2461 2841 很像. 2道定长. 重新写一下3
